ef24ab9dba6323a683685daa7ad306cc18bef5df
Daniel Oliveira <danielso2007@gmail.com>
2019-04-12 13:36:58 -0300

    fix: Correção do menu tree.

--- src/common/template/dependencies.js
+++ src/common/template/dependencies.js
@@ -1,14 +1,15 @@
+import 'modules/font-awesome/css/font-awesome.min.css';
+import 'modules/ionicons/dist/css/ionicons.min.css';
+import 'modules/admin-lte/bower_components/bootstrap/dist/css/bootstrap.min.css';
+import 'modules/admin-lte/dist/css/AdminLTE.min.css';
+import 'modules/admin-lte/dist/css/skins/_all-skins.min.css';
+
 import './jquery';
 import 'modules/admin-lte/plugins/jQueryUI/jquery-ui.min.js';
 import 'modules/admin-lte/plugins/timepicker/bootstrap-timepicker.min.js';
 import 'modules/admin-lte/plugins/bootstrap-slider/bootstrap-slider.js';
 import 'modules/admin-lte/dist/js/adminlte.min.js';
 
-import 'modules/font-awesome/css/font-awesome.min.css';
-import 'modules/ionicons/dist/css/ionicons.min.css';
-import 'modules/admin-lte/bower_components/bootstrap/dist/css/bootstrap.min.css';
-import 'modules/admin-lte/dist/css/AdminLTE.min.css';
-import 'modules/admin-lte/dist/css/skins/_all-skins.min.css';
 //import 'modules/admin-lte/plugins/iCheck/flat/blue.css';
 import './custom.css';
 import '../../auth/auth.css';
