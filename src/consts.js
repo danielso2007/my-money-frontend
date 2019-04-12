f4835816559d7a61ec9fc9c61b5cdfe5ec6cfe81
Daniel Oliveira <danielso2007@gmail.com>
2019-04-12 12:39:25 -0300

    fix: Endereço errado para /billingCycles/summary.

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,4 @@
 const url = process.env.URL_BACKEND ? process.env.URL_BACKEND : 'http://localhost:3003';
-console.log(url);
 export default {
     API_URL: `${url}/api`,
     OAPI_URL: `${url}/oapi`,
